36e5d61ee4db68ba0d9e9ccf4fd08f46cd129ea8
seokhyeong9171 <hsh9171@icloud.com>
2025-12-14 15:45:19 +0900

    Initial commit

---
+++ src/main/java/com/blogservice/BlogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.blogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogserviceApplication.class, args);
+    }
+
+}
