8c7f3ddca2876ab9808ad472a647e6490fded231
seokhyeong9171 <hsh9171@icloud.com>
2025-12-21 10:07:12 +0900

    code 필드 추가

--- src/main/java/com/blogservice/api/exception/PostNotFound.java
+++ src/main/java/com/blogservice/api/exception/PostNotFound.java
@@ -2,6 +2,7 @@ package com.blogservice.api.exception;
 
 public class PostNotFound extends CustomException {
 
+    private static final int STATUS_CODE = 404;
     private static final String MESSAGE = "존재하지 않는 글입니다.";
 
     public PostNotFound() {
@@ -14,6 +15,6 @@ public class PostNotFound extends CustomException {
 
     @Override
     public int getStatusCode() {
-        return 404;
+        return STATUS_CODE;
     }
 }
