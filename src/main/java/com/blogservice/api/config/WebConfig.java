33e562d1e36a7aa11924a15cd19c27b391d5b7c6
seokhyeong9171 <hsh9171@icloud.com>
2025-12-28 12:04:54 +0900

    secretkey bean 위치 이동

--- src/main/java/com/blogservice/api/config/WebConfig.java
+++ src/main/java/com/blogservice/api/config/WebConfig.java
@@ -1,11 +1,22 @@
 package com.blogservice.api.config;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.crypto.SecretKey;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public SecretKey secretKey(@Value("${jwt.secret}") String secret) {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
