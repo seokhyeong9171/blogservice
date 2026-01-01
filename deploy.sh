eb4b077f9137d09cba821c5c32a344af4fd3dd13
seokhyeong9171 <hsh9171@icloud.com>
2026-01-01 15:46:31 +0900

    로드밸런서 적용

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,23 @@
 #!/bin/zsh
 
 KEY_FILE="/Users/hongseoghyeong/.ssh/hong-blogservice.pem"
-SERVER_IP="43.200.225.95"
+SERVER_IPS=(43.200.225.95 16.184.56.151)
+
+if [ -f ./build/libs/app.jar ]; then
+  echo "app.jar already exist. Deleting it"
+  rm ./build/libs/app.jar
+fi
 
 ./gradlew build
 
 mv ./build/libs/*.jar ./build/libs/app.jar
 
-scp -i ${KEY_FILE} ./build/libs/app.jar ec2-user@${SERVER_IP}:~
+for SERVER_IP in "${SERVER_IPS[@]}"; do
+scp -i ${KEY_FILE} ./build/libs/app.jar ec2-user@"${SERVER_IP}":~
 
-ssh -i ${KEY_FILE} ec2-user@${SERVER_IP} \
-  "nohup java -jar app.jar > app.log 2>&1 & exit"
+ssh -i ${KEY_FILE} ec2-user@"${SERVER_IP}" <<EOF
+pkill java
+nohup java -jar app.jar > app.log 2>&1 &
+exit
+EOF
+done
